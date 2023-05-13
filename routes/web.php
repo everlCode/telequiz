c7309a542c30390f35988f449cc3648df1476447
everl <kolukul1996@gmail.com>
2023-05-13 21:56:28 +0300

    lessons

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-
+use Inertia\Inertia;
+use App\Models\Photo;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +14,42 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('photos', function () {
+    //dd(Photo::all());
+    return Inertia::render('Guest/Photos', [
+        'photos' => Photo::all(), ## 👈 Pass a collection of photos, the key will become our prop in the component
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    // other admin routes here
+    Route::get('/photos', function () {
+        return inertia('Admin/Photos', [
+            'photos' => Photo::all()
+        ]);
+    })->name('photos'); // This will respond to requests for admin/photos and have a name of admin.photos
+
+    Route::get('/photos/create', function () {
+        return inertia('Admin/PhotosCreate');
+    })->name('photos.create');
+
+    Route::post('/photos', function () {
+        dd('I will handle the form submission');   
+    })->name('photos.store');
+});
+
