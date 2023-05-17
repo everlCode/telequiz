80f21658651e834960ca2259c5f2fc513776fb86
everl <kolukul1996@gmail.com>
2023-05-17 21:53:37 +0300

    delete quiz composition api

--- resources/js/Pages/Admin/Quizzez.vue
+++ resources/js/Pages/Admin/Quizzez.vue
@@ -1,11 +1,11 @@
-<template>
+<!-- <template>
     <app-layout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Quizzez</h2>
         </template>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <DataTable :value="this.quizzezData" tableStyle="min-width: 50rem; margin-bottom: 10px">
+                <DataTable :value="title" tableStyle="min-width: 50rem; margin-bottom: 10px">
                     <Column field="id" header="Id"></Column>
                     <Column field="name" header="Name"></Column>
                     <Column field="remove" header="Remove" style="width: 5%">
@@ -51,7 +51,8 @@ import Dialog from 'primevue/dialog';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import axios from 'axios';
-
+import QuizTable from '@/Components/quizTable.vue';
+import { ref, toRef } from 'vue';
 
 export default defineComponent({
     components: {
@@ -64,17 +65,23 @@ export default defineComponent({
     props: {
         quizzez: Array,
     },
-    setup() {
+    setup(props) {
         const form = useForm({
             name: null
         })
+    
 
-        return { form }
+    // OR, turn a single property on `props` into a ref
+    const title = ref()
+
+    title.value = props.quizzez
+
+    return { title, form }
     },
     data() {
         return {
             visible: false,
-            quizzezData: this.quizzez
+            quizzezData: ref(this.quizzez)
         }
     },
     methods: {
@@ -84,9 +91,56 @@ export default defineComponent({
             axios.delete(url).then((response) => console.log(response.data.response))
                 .catch((error) => console.log(error.response.data));
 
-            delete this.quizzezData[i];
-            location.reload();
+            delete title[i];
+            //location.reload();
         }
     },
 });
+
+
+
+</script> -->
+
+
+
+
+<template>
+    <div class="card">
+        <DataTable :value="quizzez" stripedRows tableStyle="min-width: 50rem">
+            <Column field="code" header="Code"></Column>
+            <Column field="name" header="Name"></Column>
+            <Column field="category" header="Category"></Column>
+            <Column field="quantity" header="Quantity">
+                <template #body="slotProps">
+                            <Button icon="pi pi-times" severity="danger" aria-label="Cancel"
+                                @click="update()" />
+                        </template></Column>
+        </DataTable>
+    </div>
+</template>
+
+<script setup>
+
+import { ref, onMounted } from 'vue';
+import Column from 'primevue/column';
+import DataTable from 'primevue/datatable';
+import Button from 'primevue/button';
+
+
+onMounted((props) => {
+    console.log('mount')
+});
+
+function update() {
+    quizzez.value.pop();
+}
+console.log('ref')
+const quizzez = ref();
+const props =  defineProps({
+      quizzez: Array,
+   })
+quizzez.value = props.quizzez   
+
+console.log(props.quizzez)
+
 </script>
